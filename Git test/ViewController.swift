15a84d4a9b93eeefe85e02816227fd0d2de050b9
iOS Ninja <igor@remoty.com>
2018-05-05 15:24:17 +0300

    print option

--- Git test/ViewController.swift
+++ Git test/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        pintConsol(value: "Hello Wolrd")
+
+    }
+
+
+    func pintConsol(value: String) {
+        print(value)
     }
 
     override func didReceiveMemoryWarning() {
